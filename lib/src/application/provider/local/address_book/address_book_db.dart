d5e2c6da371f9ada35d9152cb24246923de58944
Nguyen Van Toan <toannv@192.168.68.54>
2024-08-01 16:22:11 +0700

    #add db model

---
+++ lib/src/application/provider/local/address_book/address_book_db.dart
@@ -0,0 +1,39 @@
+import 'package:data/data.dart';
+import 'package:isar/isar.dart';
+
+part 'address_book_db.g.dart';
+
+extension AddressBookDbExtension on AddressBookDb {
+  AddressBookDb copyWith({
+    int? id,
+    String? name,
+    String? address,
+  }) {
+    return AddressBookDb(
+      address: address ?? this.address,
+      name: name ?? this.name,
+      id: id ?? this.id,
+    );
+  }
+
+  AddressBookDto get toDto => AddressBookDto(
+        id: id,
+        address: address,
+        name: name,
+      );
+}
+
+@Collection(
+  inheritance: false,
+)
+final class AddressBookDb {
+  final String address;
+  final String name;
+  final Id id;
+
+  const AddressBookDb({
+    required this.address,
+    required this.name,
+    this.id = Isar.autoIncrement,
+  });
+}
---
+++ lib/src/application/provider/local/browser/browser_db.dart
@@ -0,0 +1,54 @@
+import 'package:data/data.dart';
+import 'package:isar/isar.dart';
+
+part 'browser_db.g.dart';
+
+extension BrowserDbExtension on BrowserDb {
+  BrowserDb copyWith({
+    Id? id,
+    String? url,
+    String? logo,
+    String? title,
+    String? screenShotUri,
+    bool? isActive,
+  }) {
+    return BrowserDb(
+      id: id ?? this.id,
+      url: url ?? this.url,
+      logo: logo ?? this.logo,
+      title: title ?? this.title,
+      screenShotUri: screenShotUri ?? this.screenShotUri,
+      isActive: isActive ?? this.isActive,
+    );
+  }
+
+  BrowserDto get toDto => BrowserDto(
+        id: id,
+        logo: logo,
+        siteTitle: title,
+        screenShotUri: screenShotUri,
+        url: url,
+        isActive: isActive,
+      );
+}
+
+@Collection(
+  inheritance: false,
+)
+final class BrowserDb {
+  final Id id;
+  final String url;
+  final String logo;
+  final String title;
+  final String screenShotUri;
+  final bool isActive;
+
+  BrowserDb({
+    required this.url,
+    required this.logo,
+    required this.title,
+    required this.screenShotUri,
+    this.id = Isar.autoIncrement,
+    required this.isActive,
+  });
+}
